1c02f2056fe38b173292980f9b4bdaf477067119
jdsiemaszko <j.d.siemaszko@student.tudelft.nl>
2023-11-08 15:36:06 +0800

    comparisons added

--- comp.py
+++ comp.py
@@ -11,30 +11,52 @@ import pHyFlow
 from pHyFlow.blobs.base.induced import vorticity
 import matplotlib.pyplot as plt
 
-case = "merge_v2_avrm_rk4"
-case_ref = "avrm_rk4"
+#---------------Current directory and paths---------------------F---------------
+arg = sys.argv
+if len(arg) > 3:
+    raise Exception("More than two arguments inserted!")
+if len(arg) <= 2:
+    raise Exception("Not enough cases to compare (need two)!")
+config1File = arg[1]
+config2File = arg[2]
 
-case_dir = os.getcwd()
-results_dir = 'results'
-comp_dir = os.path.join(case_dir, results_dir, 'comparisons')
+#-----------------------Config the yaml file ------------------
+loader = yaml.SafeLoader
+loader.add_implicit_resolver(
+    u'tag:yaml.org,2002:float',
+    re.compile(u'''^(?:
+     [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+    |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+    |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+    |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+    |[-+]?\\.(?:inf|Inf|INF)
+    |\\.(?:nan|NaN|NAN))$''', re.X),
+    list(u'-+0123456789.'))
 
-writeInterval_plots = 100
-coreSize = 'variable'
+config1 = yaml.load(open(os.path.join(config1File)),Loader=loader)
+config2 = yaml.load(open(os.path.join(config2File)),Loader=loader)
 
-data_dir = os.path.join(case_dir, results_dir, case, 'data')
-ref_dir = os.path.join(case_dir, results_dir, case_ref, 'data')
+case1 = config1['case']
 
-name_string = f'comparison_{case}_vs_{case_ref}'
+case_dir1 = os.path.join(os.getcwd(), 'results', case1)
+data_dir1 = os.path.join(case_dir1,config1["data_folder"])
+plots_dir1= os.path.join(case_dir1,config1["plots_folder"])
+
+
+case2 = config2['case']
+
+case_dir2 = os.path.join(os.getcwd(), 'results', case2)
+data_dir2 = os.path.join(case_dir2,config2["data_folder"])
+plots_dir2 = os.path.join(case_dir2,config2["plots_folder"])
+
+comp_dir = os.path.join(os.getcwd(), 'results', 'comparisons')
+
+name_string = f'comparison_{case1}_vs_{case2}'
 plots_dir =  os.path.join(comp_dir, name_string)
 Path(plots_dir).mkdir(parents=True, exist_ok=True)
 
-# uxNorm = np.array([])
-# uyNorm = np.array([])
-# omegaNorm = np.array([])
-# t_norm = np.array([])
-#Line plots
-times_file = os.path.join(data_dir,"times_{}.csv".format(case))
-times_ref_file = os.path.join(ref_dir,"times_{}.csv".format(case_ref))
+times_file = os.path.join(data_dir1,"times_{}.csv".format(case1))
+times_ref_file = os.path.join(data_dir2,"times_{}.csv".format(case2))
 times_data = pandas.read_csv(times_file)
 times_ref_data = pandas.read_csv(times_ref_file)
 
@@ -53,8 +75,8 @@ gammaC = circulation2[0]
 deltaTc = time2[2] - time2[1]
 
 fig, ax = plt.subplots(1,1,figsize=(6,6))
-ax.plot(time1,noBlobs1, label=case)
-ax.plot(time2,noBlobs2, label=case_ref)
+ax.plot(time1,noBlobs1, label=case1)
+ax.plot(time2,noBlobs2, label=case2)
 
 plt.grid(color = '#666666', which='major', linestyle = '--', linewidth = 0.5)
 plt.minorticks_on()
@@ -63,11 +85,11 @@ plt.title('Total number of particles')
 plt.ylabel('Particles')
 plt.xlabel('time $(sec)$')
 plt.legend()
-plt.savefig("{}/number_of_particles_{}.png".format(plots_dir,case), dpi=300, bbox_inches="tight")
+plt.savefig("{}/number_of_particles_{}.png".format(plots_dir,case1), dpi=300, bbox_inches="tight")
 
 fig, ax = plt.subplots(1,1,figsize=(6,6))
-ax.plot(time1,circulation1- gammaC, label=case)
-ax.plot(time2,circulation2- gammaC, label=case_ref)
+ax.plot(time1,circulation1- gammaC, label=case1)
+ax.plot(time2,circulation2- gammaC, label=case2)
 
 plt.grid(color = '#666666', which='major', linestyle = '--', linewidth = 0.5)
 plt.minorticks_on()
@@ -76,18 +98,18 @@ plt.title('Circulation')
 plt.ylabel('circulation')
 plt.xlabel('time $(sec)$')
 plt.legend()
-plt.savefig("{}/circulation_{}.png".format(plots_dir,case), dpi=300, bbox_inches="tight")
+plt.savefig("{}/circulation_{}.png".format(plots_dir,case1), dpi=300, bbox_inches="tight")
 
 fig = plt.subplots(figsize=(6,6))
 index = np.arange(len(evolution_time1))
 width = deltaTc
-lagrangian = plt.bar(index[1:nTimeSteps]*deltaTc, evolution_time1[1:nTimeSteps], width, label=case, alpha=0.5)
-lagrangian = plt.bar(index[1:nTimeSteps]*deltaTc, evolution_time2[1:nTimeSteps], width, label=case_ref, alpha=0.5)
+lagrangian = plt.bar(index[1:nTimeSteps]*deltaTc, evolution_time1[1:nTimeSteps], width, label=case1, alpha=0.5)
+lagrangian = plt.bar(index[1:nTimeSteps]*deltaTc, evolution_time2[1:nTimeSteps], width, label=case2, alpha=0.5)
 plt.legend()
 plt.ylabel('Timestep Time (s)')
 plt.xlabel('Simulation time(s)')
 plt.title('Evolution Times')
-plt.savefig("{}/times_{}.png".format(plots_dir,case), dpi=300, bbox_inches="tight")
+plt.savefig("{}/times_{}.png".format(plots_dir,case1), dpi=300, bbox_inches="tight")
 
 fig = plt.subplots(figsize=(6,6))
 index = np.arange(len(evolution_time1))
@@ -96,6 +118,202 @@ lagrangian = plt.bar(index[1:nTimeSteps]*deltaTc, evolution_time1[1:nTimeSteps] 
 
 plt.ylabel('Timestep Time (s)')
 plt.xlabel('Simulation time(s)')
-plt.title(f'Evolution Time Difference, {case} - {case_ref}')
-plt.savefig("{}/times_dif_{}.png".format(plots_dir,case), dpi=300, bbox_inches="tight")
+plt.title(f'Evolution Time Difference, {case1} - {case2}')
+plt.savefig("{}/times_dif_{}.png".format(plots_dir,case1), dpi=300, bbox_inches="tight")
+
+writeInterval_plots1 = config1["writeInterval_plots"]
+writeInterval_plots2 = config2["writeInterval_plots"]
+
+coreSize1 = config1["coreSize"]
+coreSize2 = config2["coreSize"]
+
+nPlotPoints1 = config1["nPlotPoints"]
+xMinPlot1 = config1["xMinPlot"]
+xMaxPlot1 = config1["xMaxPlot"]
+yMinPlot1 = config1["yMinPlot"]
+yMaxPlot1 = config1["yMaxPlot"]
+plotting_params1 = [writeInterval_plots1, nPlotPoints1, xMinPlot1, xMaxPlot1, yMinPlot1, yMaxPlot1]
+
+nPlotPoints2 = config2["nPlotPoints"]
+xMinPlot2 = config2["xMinPlot"]
+xMaxPlot2 = config2["xMaxPlot"]
+yMinPlot2 = config2["yMinPlot"]
+yMaxPlot2 = config2["yMaxPlot"]
+plotting_params2 = [writeInterval_plots2, nPlotPoints2, xMinPlot2, xMaxPlot2, yMinPlot2, yMaxPlot2]
+
+plotting_param_mishmatch = False
+for p1, p2 in zip(plotting_params1, plotting_params2):
+    if p1 != p2:
+        plotting_param_mishmatch = True
+        print(f'detected a mismatch in plotting params!')
+
+time = []
+L2error = []
+Linferror = []
+
+if not plotting_param_mishmatch:
+    for timeStep in range(0, nTimeSteps+1):
+        if timeStep%writeInterval_plots1 == 0:
+            file1 = os.path.join(data_dir1,'results_{}_{n:06d}.csv'.format(case1,n=timeStep))
+            data1 = np.genfromtxt(file1)
+
+            file2 = os.path.join(data_dir2,'results_{}_{n:06d}.csv'.format(case2,n=timeStep))
+            data2 = np.genfromtxt(file2)
+
+            xplot = data1[:,0]
+            yplot = data1[:,1]
+            length = int(np.sqrt(len(xplot)))
+            xPlotMesh = xplot.reshape(length,length)
+            yPlotMesh = yplot.reshape(length,length)
+
+            ux1 = data1[:,2]
+            uy1 = data1[:,3]
+            omega1 = data1[:,4]
+
+            ux2 = data2[:,2]
+            uy2 = data2[:,3]
+            omega2 = data2[:,4]
+
+            # xTicks = np.linspace(-xMinPlot1, xMaxPlot1, 5)
+            # yTicks = np.linspace(-yMinPlot1, yMaxPlot1, 5)
+
+            fig, ax = plt.subplots(1,1,figsize=(6,6))
+            ax.set_aspect("equal")
+            # ax.set_xticks(xTicks)
+            # ax.set_yticks(yTicks)
+            plt.grid(color = '#666666', which='major', linestyle = '--', linewidth = 0.5)
+            plt.minorticks_on()
+            plt.grid(visible=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            cax = ax.contourf(xPlotMesh,yPlotMesh,np.abs((omega1.reshape(length,length) - omega2.reshape(length, length))) / max(np.abs(omega2)),levels=100,cmap='RdBu',extend="both")
+            cbar = fig.colorbar(cax,format="%.4f")
+            cbar.set_label("Vorticity Error (%)")
+            plt.tight_layout()
+            plt.savefig("{}/vorticity_error_%_{}_vs_{}_{}.png".format(plots_dir,case1,case2, timeStep), dpi=300, bbox_inches="tight")
+            plt.close(fig)
+            
+            fig, ax = plt.subplots(1,1,figsize=(6,6))
+            ax.set_aspect("equal")
+            # ax.set_xticks(xTicks)
+            # ax.set_yticks(yTicks)
+            plt.grid(color = '#666666', which='major', linestyle = '--', linewidth = 0.5)
+            plt.minorticks_on()
+            plt.grid(visible=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            abs_vel_error = np.sqrt((ux1.reshape(length,length) - ux2.reshape(length, length))**2 + (uy1.reshape(length,length) - uy2.reshape(length, length))**2)
+            vel_magnitude = np.sqrt(ux2.reshape(length,length)**2+uy2.reshape(length,length)**2)
+            cax = ax.contourf(xPlotMesh,yPlotMesh,abs_vel_error / np.max(vel_magnitude),levels=100,cmap='RdBu',extend="both")
+            cbar = fig.colorbar(cax,format="%.4f")
+            cbar.set_label("Velocity Error (%)")
+            plt.tight_layout()
+            plt.savefig("{}/velocity_error_%_{}_vs_{}_{}.png".format(plots_dir,case1, case2,timeStep), dpi=300, bbox_inches="tight")
+            plt.close(fig)
+
+            time.append(timeStep)
+            L2error.append(np.linalg.norm(omega1-omega2) / np.linalg.norm(omega2))
+            Linferror.append(max(np.abs(omega1-omega2)) / max(np.abs(omega2)))
+
+else:
+    print(f'skipping vorticity and velocity plots due to plotting parameter mismatch!')
+    # for timeStep in range(nTimeSteps+1):
+    #     if timeStep%writeInterval_plots == 0:
+
+    #         blobs_file = os.path.join(data_dir,'blobs_{}_{n:06d}.csv'.format(case,n=timeStep))
+    #         blobs_data = np.genfromtxt(blobs_file)
+    #         blobs_x = blobs_data[:,0]
+    #         blobs_y = blobs_data[:,1]
+    #         blobs_g = blobs_data[:,2]
+    #         if coreSize == 'variable':
+    #             blobs_sigma = blobs_data[:,3]
+    #         else:
+    #             blobs_sigma = sigma_ref
+    #         # print(blobs_x, blobs_y, blobs_g, blobs_sigma)
+
+    #         blobs_ref_file = os.path.join(ref_dir,'blobs_{}_{n:06d}.csv'.format(case_ref,n=timeStep))
+    #         blobs_ref_data = np.genfromtxt(blobs_ref_file)
+    #         ref_x = blobs_ref_data[:,0]
+    #         ref_y = blobs_ref_data[:,1]
+    #         ref_g = blobs_ref_data[:,2]
+    #         if coreSize == 'variable':
+    #             ref_sigma = blobs_ref_data[:,3]
+    #         else:
+    #             ref_sigma = sigma_ref
+    #         # print(ref_x, ref_y, ref_g, ref_sigma)
+
+    #         # print(sum(blobs_g) - sum(ref_g))
+
+
+    #         omega = vorticity(np.array(blobs_x), np.array(blobs_y), np.array(blobs_g), np.array(blobs_sigma), xEval = np.array(xplotflat), yEval = np.array(yplotflat))
+    #         omega_ref = vorticity(np.array(ref_x), np.array(ref_y), np.array(ref_g), np.array(ref_sigma), xEval = np.array(xplotflat), yEval = np.array(yplotflat))
+    #         # print(omega, omega_ref)
+    #         # print(np.linalg.norm(omega-omega_ref))
+    #         # print(max(omega-omega_ref), max(np.abs(omega_ref)), max(np.abs(omega)))
+    #         time.append(timeStep)
+    #         L2error.append(np.linalg.norm(omega-omega_ref))
+    #         Linferror.append(max(np.abs(omega-omega_ref)))
+            
+    #         xPlotMesh = xplotflat.reshape(length, length)
+    #         yPlotMesh = yplotflat.reshape(length, length)
+    #         # xTicks = np.linspace(-2,2,5)
+    #         # yTicks = np.linspace(-2,2,5)
+
+    #         fig, ax = plt.subplots(1,1,figsize=(12,6))
+    #         ax.set_aspect("equal")
+    #         # ax.set_xticks(xTicks)
+    #         # ax.set_yticks(yTicks)
+    #         plt.grid(color = '#666666', which='major', linestyle = '--', linewidth = 0.5)
+    #         plt.minorticks_on()
+    #         plt.grid(visible=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+    #         ax.set_xlabel("x")
+    #         ax.set_ylabel("y")
+    #         # BEWARE OF SKETCHY SOLUTION FOR LEVELS
+    #         e_abs = (omega.reshape(length,length) - omega_ref.reshape(length,length))
+    #         e_abs_max = np.max(np.abs(e_abs))
+    #         step = e_abs_max / 50
+    #         try:
+    #             cax = ax.contourf(xPlotMesh,yPlotMesh, e_abs ,levels=np.arange(-e_abs_max, e_abs_max+step, step),cmap='RdBu',extend="both")
+    #             cbar = fig.colorbar(cax,format="%.4f")
+    #             cbar.set_label("Absolute Vorticity Error (1/s)")
+    #             plt.tight_layout()
+    #             plt.savefig("{}/absolute_vorticity_error_{}_{}.png".format(plots_dir,case,timeStep), dpi=300, bbox_inches="tight")
+    #             plt.close(fig)
+    #         except:
+    #             pass
+
+    #         fig, ax = plt.subplots(1,1,figsize=(12,6))
+    #         ax.set_aspect("equal")
+    #         # ax.set_xticks(xTicks)
+    #         # ax.set_yticks(yTicks)
+    #         plt.grid(color = '#666666', which='major', linestyle = '--', linewidth = 0.5)
+    #         plt.minorticks_on()
+    #         plt.grid(visible=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+    #         ax.set_xlabel("x")
+    #         ax.set_ylabel("y")
+    #         # BEWARE OF SKETCHY SOLUTION FOR LEVELS
+    #         e_rel = (omega.reshape(length,length) - omega_ref.reshape(length,length))/ max(omega_ref) * 100
+    #         e_rel_max = np.max(np.abs(e_rel))
+    #         step_rel = e_rel_max/50
+    #         try:
+    #             cax = ax.contourf(xPlotMesh,yPlotMesh, e_rel ,levels=np.arange(-e_rel_max, e_rel_max+step_rel, step_rel),cmap='RdBu',extend="both")
+    #             cbar = fig.colorbar(cax,format="%.4f")
+    #             cbar.set_label("Relative Vorticity Error (%)")
+    #             plt.tight_layout()
+    #             plt.savefig("{}/relative_vorticity_error_{}_{}.png".format(plots_dir,case,timeStep), dpi=300, bbox_inches="tight")
+    #             plt.close(fig)
+    #         except:
+    #             pass
+
+plt.clf()
+fig, ax1 = plt.subplots(figsize=(6,6))
+ax1.plot(time, L2error, label='L2', color = 'tab:blue', marker='x')
+ax2 = ax1.twinx()
+ax2.plot(time, Linferror, label='Linf', color='tab:red', marker='x')
+ax1.set_xlabel('Timestep')
+ax1.set_ylabel('L2 Error', color='tab:blue')
+ax2.set_ylabel('Linf error', color='tab:red')
+plt.title('Error Evolution Over Time')
+plt.savefig("{}/error_evolution_{}_vs_{}.png".format(plots_dir,case1, case2), dpi=300, bbox_inches="tight")
+
 
